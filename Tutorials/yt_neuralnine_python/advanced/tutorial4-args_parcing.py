ed4b145557e6395a95fd3e713040bcf35300f839
Mateusz <=>
2024-04-10 18:54:26 +0200

    T4 done

--- Tutorials/yt_neuralnine_python/advanced/tutorial4-args_parcing.py
+++ Tutorials/yt_neuralnine_python/advanced/tutorial4-args_parcing.py
@@ -1,10 +1,11 @@
 import sys 
 import getopt
 
+# Easy way
 # fname = sys.argv[1]
 # msg = sys.argv[2]
 
-
+# Proper way
 opts, args = getopt.getopt(sys.argv[1:], 'f:m:', ['fname', 'msg'])
 
 
@@ -14,8 +15,16 @@ print(opts)
 # ['log.file', 'hellow world']
 # []
 
-# python3 tutorial4-args_parcing.py -f log.file -m hellow\ world
+# python3 tutorial4-args_parcing.py -m hellow\ world -f log.file # changed order
 print(args)
 print(opts)
 # []
 # [('-f', 'log.file'), ('-m', 'hellow world')]
+# access
+for key, value in opts:
+    if key == '-f':
+        fname = value
+    elif key == '-m':
+        msg = value
+    else:
+        print(f'Unknown: {key}={value}')
