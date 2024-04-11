1910e91ef4eeb8c8588afd34fd41b4ebc5bba944
Mateusz <=>
2024-04-11 13:04:27 +0200

    T7 done

---
+++ Tutorials/yt_neuralnine_python/advanced/tutorial7-factory_design_pattern.py
@@ -0,0 +1,55 @@
+from abc import ABCMeta, abstractmethod
+
+class IPerson(metaclass=ABCMeta):
+    '''
+    meta=ABCMEtda means that this is a Abstract class and we cannot create objects from it
+    '''
+    @staticmethod 
+    @abstractmethod
+    def person_method():
+        '''Interface method'''
+
+# # this doesn't work: TypeError: Can't instantiate abstract class IPerson with abstract method person_method
+# p1 = IPerson()
+# p1.person_method()
+
+# new class
+class Student(IPerson):
+    def __init__(self) -> None:
+        self.name = 'Basic Student Name'
+    
+    def person_method(self):
+        """
+        This method needs to be overridden we use it or not because it's an abstract method. 
+        """
+        print('im student')
+
+class Teacher(IPerson):
+    def __init__(self) -> None:
+        self.name = 'Basic Teacher name'
+
+    def person_method(self):
+        print('im teacher')
+
+# s1 = Student()
+# s1.person_method()
+# t1 = Teacher()
+# t1.person_method()
+
+class PersonFactory:
+    @staticmethod
+    def build_person(person_type: str):
+        if person_type == 'Student':
+            return Student()
+        elif person_type == 'Teacher':
+            return Teacher()
+        else:
+            raise Exception('Invalid person type')
+# s1 = PersonFactory.build_person('student')
+# t1 = PersonFactory.build_person('teacher')
+
+if __name__ == '__main__':
+
+    choice = input('What type of person do you want?\n')
+    p1 = PersonFactory.build_person(choice)
+    p1.person_method()
